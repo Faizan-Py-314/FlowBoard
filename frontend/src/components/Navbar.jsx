5ea3c8be790c87f9d4a9e0273ecba919c1e80598
Faizan-Py-314 <pydeveloper314@gmail.com>
2026-08-20 11:19:11 +0500

    Create Navbar component in frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { RiSwap3Fill, RiCloseFill, RiHome9Fill, RiTodoLine, RiTrelloLine, RiCalendarTodoLine } from "@remixicon/react";
+
+
+const Navbar = () => {
+    const [activeOption, setActiveOption] = useState('Home')
+    const [navActive, setNavActive] = useState(true)
+
+  return (
+    <>
+      <div className={`p-4 w-full h-svh brs ${navActive? 'block':'hidden'} lg:w-1/5 lg:px-3 2xl:w-1/6`}>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center gap-2'>
+            <RiSwap3Fill size={40} />
+            <h2 className='font-bold text-2xl'>Flow Board</h2>
+          </div>
+          <span onClick={() => setNavActive(false)} className='lg:hidden'><RiCloseFill size={30}/></span>
+        </div>
+          <hr className='my-3'/>
+        <ul className='px-2 flex flex-col gap-1 lg:p-0 xl:gap-2'>
+
+          <span onClick={() => setActiveOption('Home')} className={`flex gap-3 items-center px-3 py-2 rounded-md cursor-pointer xl:py-3 ${activeOption == 'Home'? 'active':''} hover:bg-[#F3F3F3] lg:px-2 xl:px-3`}>
+            <RiHome9Fill size={20}/>
+            <li><a href="#">Dashboard</a></li>
+          </span>
+
+          <span onClick={() => setActiveOption('Tasks')} className={`flex justify-between items-center px-3 py-2 rounded-md cursor-pointer xl:py-3 ${activeOption == 'Tasks'? 'active':''} hover:bg-[#F3F3F3] lg:px-2 xl:px-3`}>
+            <span className='flex gap-3 items-center'>
+              <RiTodoLine size={20}/>
+              <li><a href="#">Today Tasks</a></li>
+            </span>
+            <span className='text-sm mr-2'>4</span>
+          </span>
+
+          <span onClick={() => setActiveOption('Projects')} className={`flex justify-between items-center px-3 py-2 rounded-md cursor-pointer xl:py-3 ${activeOption == 'Projects'? 'active':''} hover:bg-[#F3F3F3] lg:px-2 xl:px-3`}>
+            <span className='flex gap-3 items-center'>
+              <RiTrelloLine size={20}/>
+              <li><a href="#">All Projects</a></li>
+            </span>
+            <span className='text-sm mr-2'>9</span>
+          </span>
+
+          <span onClick={() => setActiveOption('Schedule')} className={`flex gap-3 items-center px-3 py-2 rounded-md cursor-pointer xl:py-3 ${activeOption == 'Schedule'? 'active':''} hover:bg-[#F3F3F3] lg:px-2 xl:px-3`}>
+            <RiCalendarTodoLine size={20}/>
+            <li><a href="#">Study Schedule</a></li>
+          </span>
+          
+        </ul>
+      </div>
+      
+    </>
+  )
+}
+
+export default Navbar
