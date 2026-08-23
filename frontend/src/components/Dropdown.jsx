b5ca083d0f3596a4afddc3bd3b2fdbb1dad1b478
Faizan-Py-314 <pydeveloper314@gmail.com>
2026-08-23 15:36:43 +0500

    Create search component in frontend

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { RiArrowDropDownLine } from "@remixicon/react";
 
 
-const Dropdown = ({ title = "Select Option", items = [] }) => {
+const Dropdown = ({ className = '', title = "Select Option", items = [], postion = 'right' }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [checkedItems, setCheckedItems] = useState(new Array(items.length).fill(false));
   const dropdownRef = useRef(null);
@@ -20,7 +20,6 @@ const Dropdown = ({ title = "Select Option", items = [] }) => {
     const handleCheckboxChange = (index) => {
     const nextState = !checkedItems[index];
 
-    // 1. Update component state
     setCheckedItems((prev) => {
       const updated = [...prev];
       updated[index] = nextState;
@@ -35,15 +34,15 @@ const Dropdown = ({ title = "Select Option", items = [] }) => {
 
   return (
     <div className="relative inline-block text-left" ref={dropdownRef}>
-      <button onClick={() => setIsOpen(!isOpen)} className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 border border-gray-300 hover:bg-gray-50 focus:outline-none" aria-expanded={isOpen}> {title} <RiArrowDropDownLine className={`-mr-1 h-5 w-5 text-gray-400 transition-transform duration-200 ${isOpen ? "rotate-180" : ""}`} /> </button>
+      <button onClick={() => setIsOpen(!isOpen)} className={`${className} inline-flex w-full justify-center items-center gap-x-1.5 rounded-md bg-white px-4 py-2 text-sm text-gray-900 border border-gray-300 hover:bg-gray-50 focus:outline-none`} aria-expanded={isOpen}> {title} <RiArrowDropDownLine className={`-mr-1 h-5 w-5 text-gray-400 transition-transform duration-200 ${isOpen ? "rotate-180" : ""}`} /> </button>
 
       
-        <div className={`${isOpen? 'absolute':'hidden'} right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white border`}>
+        <div className={`${isOpen? 'absolute':'hidden'} ${postion}-0 z-10 mt-2 min-w-40 origin-top-right rounded-md bg-white border border-gray-300 transition-transform`}>
             <div className="py-1" role="none">
             {items.map((item, index) => (
                 <label onClick={() => {setIsOpen(false); }} key={index} htmlFor={`checkbox-${index}`} className="flex items-center gap-2 px-4 py-2 hover:bg-gray-100 hover:text-gray-900 transition-colors">
                     <input onChange={() => handleCheckboxChange(index)} checked={checkedItems[index || false]} className="accent-black" id={`checkbox-${index}`} type="checkbox"></input>
-                    <span htmlFor={item.label} className="block w-full text-left text-sm text-gray-700 " role="menuitem">{item.label}</span>
+                    <span htmlFor={item.label} className="block w-full text-left text-sm text-gray-500 " role="menuitem">{item.label}</span>
                 </label>
             ))}
             </div>
--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -12,13 +12,14 @@ const Search = () => {
   ];
 
   return (
-    <div className='flex'>
-        <div className='flex items-center gap-2 border border-gray-300 rounded-md px-2 py-1 text-sm'>
+    <div className='mx-5 mt-3 md:flex md:items-center w-[90%] md:w-[90%] md:mt-4 md:mr-0 md:gap-2'>
+        <div className='flex items-center gap-2 border border-gray-300 rounded-md px-2 py-2 text-sm w-full md:w-[60%] md:text-base'>
             <RiSearchLine className='w-5.5 h-5.5 text-gray-500' />
             <input className='[&::-webkit-search-cancel-button]:appearance-none focus:outline-none w-full' type="search" placeholder='Search' />
         </div>
-        <div>
-            <Dropdown title="Level" items={levelOptions} />
+        <div className='flex gap-2 mt-2 md:mt-0'>
+            <Dropdown className='text-xs h-8 md:text-sm md:h-10 ' postion='left' title="Level" items={levelOptions} />
+            <Dropdown className='text-xs h-8 md:text-sm md:h-10' title="Status" items={levelOptions} />
         </div>
     </div>
   )
