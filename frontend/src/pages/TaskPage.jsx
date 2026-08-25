664985bb63f0423e412f35522d5aa97c01edc3e5
Faizan-Py-314 <pydeveloper314@gmail.com>
2026-08-25 17:21:57 +0500

    Create Task page in frontend

--- frontend/src/pages/TaskPage.jsx
+++ frontend/src/pages/TaskPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Search from '../components/Search'
+import TaskCard from '../components/TaskCard';
 
 const TaskPage = () => {
   return (
     <>
       <Search />
+      <div className='p-4 sm:p-6 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 max-w-8xl mx-auto'>
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
+          <TaskCard />
+      </div>
     </>
   )
 }
