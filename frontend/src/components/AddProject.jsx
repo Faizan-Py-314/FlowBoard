dead1b569700c1d73537a6d45fc63e4ad29bb014
Faizan-Py-314 <pydeveloper314@gmail.com>
2026-09-01 11:46:26 +0500

    Complete AddProject component with all login

--- frontend/src/components/AddProject.jsx
+++ frontend/src/components/AddProject.jsx
@@ -1,7 +1,11 @@
-import React, { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import { RiArrowLeftSLine, RiArrowRightSLine } from '@remixicon/react'
 
 const AddProject = () => {
     const [requirements, setRequirements] = useState(['', ''])
+    const [levelTag, setLevelTag] = useState('Beginner')
+    const [levelCount, setLevelCount] = useState(0)
+    const [formData, setFormData] = useState({name: '', description: '', tags: ''})
 
     const containerRef = useRef(null);
 
@@ -13,27 +17,59 @@ const AddProject = () => {
             });
         };
     }, [requirements])
+    
+    const levels = ['Beginner', 'Intermediate', 'Advanced']
+
+    useEffect(() => {
+      setLevelTag(levels[levelCount])
+    }, [levelCount])
+
+    const handleChangeRequirements = (e, index) => {
+      const newValue = e.target.value
+      const newRequirements = [...requirements]
+      newRequirements[index] = newValue
+      setRequirements(newRequirements)
+    }
+
+    const handleChange = (e) => {
+      setFormData({...formData, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      const tagsList = []
+      if (formData.tags.length != 0) {
+        for (const item of formData.tags.split(', ')) {tagsList.push(item)}
+      }
+      tagsList.unshift(levelTag, 'Future')
+      console.log(formData.name, formData.description, requirements, tagsList);
+    }
 
   return (
-    <div className='absolute w-full h-[80svh] flex items-center z-30 justify-between'>
-      <div className='mx-auto bg-white p-4 rounded-md flex flex-col gap-2 text-sm w-82 border md:text-base md:w-120 md:p-6 md:gap-3'>
+    <div className='absolute w-full h-[85svh] flex items-center z-30 justify-between'>
+      <form onSubmit={handleSubmit} className='mx-auto bg-white p-4 rounded-md flex flex-col gap-2 text-sm w-82 border md:text-base md:w-120 md:p-6 md:gap-3'>
       <h2 className='text-2xl font-bold ml-2'>Add New Project</h2>
-        <input className=' focus:outline-none border border-gray-300 py-2 px-3 w-full rounded-md' type="text" placeholder='Project Name' />
-        <textarea className=' focus:outline-none border border-gray-300 py-2 px-3 w-full rounded-md' rows='4' type="text" placeholder='Project Description'></textarea>
+        <div className='flex justify-between items-center  border border-gray-300 rounded-md'>
+          <button type='button' onClick={() => setLevelCount(levelCount>=1?levelCount-1:levelCount)} className='border-r border-gray-300 cursor-pointer hover:bg-gray-100 p-2'><RiArrowLeftSLine className='w-4 h-4 md:w-5.5 md:h-5.5' /></button>
+          <input className='w-fit text-center caret-transparent focus:outline-none' type='text' value={levelTag} readOnly />
+          <button type='button' onClick={() => setLevelCount(levelCount<2?levelCount+1:levelCount)} className='border-l border-gray-300 cursor-pointer hover:bg-gray-100 p-2'><RiArrowRightSLine className='w-4 h-4 md:w-5.5 md:h-5.5' /></button>
+        </div>
+        <input onChange={handleChange} className=' focus:outline-none border border-gray-300 py-2 px-3 w-full rounded-md' type="text" name='name' value={formData.name} placeholder='Project Name' />
+        <textarea onChange={handleChange} className=' focus:outline-none border border-gray-300 py-2 px-3 w-full rounded-md' rows='4' type="text" name='description' value={formData.description} placeholder='Project Description'></textarea>
 
         <div className='p-3 border border-gray-400 rounded-md flex flex-col gap-1 pb-2 md:gap-2'>
           <span className='ml-2 font-bold text-sm md:-mb-1'>Requirements for project</span>
           <div ref={containerRef} className='flex flex-col gap-2 h-21.5 overflow-auto no_scrollbar md:h-23'>
             {requirements.map((requirement, index) => {
-                return <input key={index} className=' focus:outline-none border border-gray-300 py-2 px-3 w-full rounded-md' type="text" placeholder={`Requirement ${index+1}`} />
+                return <input onChange={(e) => {handleChangeRequirements(e, index)}} key={index} value={requirement} className=' focus:outline-none border border-gray-300 py-2 px-3 w-full rounded-md' type="text" placeholder={`Requirement ${index+1}`} />
             })}
           </div>
-        <button onClick={() => {setRequirements([...requirements, ''])}} className='w-full p-2 bg-black text-white rounded-md my-1 cursor-pointer'>Add Requirement</button>
+        <button type='button' onClick={() => {setRequirements([...requirements, ''])}} className='w-full p-2 bg-black text-white rounded-md my-1 cursor-pointer'>Add Requirement</button>
 
         </div>
-        <input className=' focus:outline-none border border-gray-300 py-2 px-3 w-full rounded-md' type="text" placeholder='Tag1, Tag2, Tag3...' />
-        <button className='w-full p-2 bg-black text-white rounded-md cursor-pointer'>Create Project</button>
-      </div>
+        <input onChange={handleChange} className=' focus:outline-none border border-gray-300 py-2 px-3 w-full rounded-md' type="text" name='tags' value={formData.tags} placeholder='Tag1, Tag2, Tag3...' />
+        <button type='submit' className='w-full p-2 bg-black text-white rounded-md cursor-pointer'>Create Project</button>
+      </form>
     </div>
   )
 }
